2807d4540a38eae44154283991527659d8844f7c
Sandor Ivanyo <139584544+SancosJak@users.noreply.github.com>
2024-06-07 22:36:23 +0200

    update todo tasks

--- src/main/java/loolu/loolu_backend/TODO_Tasks.java
+++ src/main/java/loolu/loolu_backend/TODO_Tasks.java
@@ -3,22 +3,11 @@ package loolu.loolu_backend;
 public class TODO_Tasks {
     //TODO
 
-    //make user domain main, user model - delete from code
-    //db update by new user and fields and connection to db
-
     //delete user - Slava, Aleksandru
     //put user -Slava, Aleksandru
 
     //active profile check user ??
 
-    //find products by name    - done by me
-
-    //find products by category - Radu, Sergey
-    //find all products by price between 2 users price min, max - Radu, Sergey
-    //find all products by price between 2 users price min max by name - Radu, Sergey
-
-    //connect digital ocean????
-    // or s3 ?
-    // memo in swagger fix?  dto only
+    //check connection - Slava one-one, one-many, many-one, many-many
 
 }
