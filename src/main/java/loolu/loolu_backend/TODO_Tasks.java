ea02bec342b51892cc084444c665fcb316801f8f
Sandor Ivanyo <139584544+SancosJak@users.noreply.github.com>
2024-06-09 21:12:58 +0200

    update project

--- src/main/java/loolu/loolu_backend/TODO_Tasks.java
+++ src/main/java/loolu/loolu_backend/TODO_Tasks.java
@@ -10,4 +10,9 @@ public class TODO_Tasks {
 
     //check connection - Slava one-one, one-many, many-one, many-many
 
+    //upload files photo, size check by
+
+    //@Column(name = "picture", nullable = false) class Product
+    //    private String picture;  make pictureS array max 4 photo, min 2 - update db
+
 }
