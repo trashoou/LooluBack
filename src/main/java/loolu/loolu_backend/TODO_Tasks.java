cb856c6a994deb08c6a745369320002c2f663dfd
Sandor Ivanyo <139584544+SancosJak@users.noreply.github.com>
2024-06-10 15:09:49 +0200

    update todo tasks

--- src/main/java/loolu/loolu_backend/TODO_Tasks.java
+++ src/main/java/loolu/loolu_backend/TODO_Tasks.java
@@ -8,14 +8,16 @@ public class TODO_Tasks {
     //delete user - Slava, Aleksandru
     //put user -Slava, Aleksandru
 
+    //DB for ROLE???
+
     //active profile check user ??
 
     //check connection - Slava one-one, one-many, many-one, many-many
 
-    //upload files photo, size check by
+    //upload files photo, size check by - Radu, Sergey
 
     //@Column(name = "picture", nullable = false) class Product
     // private String picture;  make pictureS array max 4 photo, min 2 - update db - Aleksandru
 
-    //update all connections in
+    //update all connections in db, classes - Slava
 }
