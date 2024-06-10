a5b1066360034fb42c09d3b9c7d3c0bfbfc4140f
Sandor Ivanyo <139584544+SancosJak@users.noreply.github.com>
2024-06-10 20:14:12 +0200

    update todo

--- src/main/java/loolu/loolu_backend/TODO_Tasks.java
+++ src/main/java/loolu/loolu_backend/TODO_Tasks.java
@@ -3,21 +3,15 @@ package loolu.loolu_backend;
 public class TODO_Tasks {
     //TODO
 
-    //user controller - all methods
+    //DB for ROLEn - ???
 
-    //delete user - Slava, Aleksandru
-    //put user -Slava, Aleksandru
-
-    //DB for ROLE???
-
-    //active profile check user ??
-
-    //check connection - Slava one-one, one-many, many-one, many-many
+    // check and update connection - Slava one-one, one-many, many-one, many-many
 
     //upload files photo, size check by - Radu, Sergey
 
-    //@Column(name = "picture", nullable = false) class Product
-    // private String picture;  make pictureS array max 4 photo, min 2 - update db - Aleksandru
+    // @Column(name = "picture", nullable = false) class Product
+    // private String picture;  make pictureS array max 4 photo, min 2 - update db  - Sasha
 
-    //update all connections in db, classes - Slava
+    // liquibase base  - Alexandru
+    // https://www.baeldung.com/liquibase-refactor-schema-of-java-app
 }
